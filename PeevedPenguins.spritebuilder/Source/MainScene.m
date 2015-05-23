c3074d274a3d83806f31bff9a5e7d9a8777f2549
Rafael Adonay Tinajero San Emeterio <radonay@MBP-de-Rafael.lan>
2015-05-23 16:16:09 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/MainScene.m
+++ PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -14,6 +14,9 @@
 
 - (void)play {
     CCLOG(@"play button pressed");
+    
+    CCScene *gameplayScene = [CCBReader loadAsScene:@"Gameplay"];
+    [[CCDirector sharedDirector] replaceScene:gameplayScene];
 }
 
 @end
