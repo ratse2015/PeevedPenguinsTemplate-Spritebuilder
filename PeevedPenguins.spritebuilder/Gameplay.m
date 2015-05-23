055c0d3c56e21190bde4b1008551cf6ff830d3d0
Rafael Adonay Tinajero San Emeterio <radonay@MBP-de-Rafael.lan>
2015-05-23 18:39:40 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -13,11 +13,14 @@
     CCNode *_levelNode;
     CCNode *_catapultArm;
      CCNode *_contentNode;
+    CCNode *_pullbackNode;
 }
 
 
 // is called when CCB file has completed loading
 - (void)didLoadFromCCB {
+    // nothing shall collide with our invisible nodes
+    _pullbackNode.physicsBody.collisionMask = @[];
     // visualize physics bodies & joints
     _physicsNode.debugDraw = TRUE;
     // tell this scene to accept touches
